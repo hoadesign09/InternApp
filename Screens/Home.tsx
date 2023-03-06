07cb9186737e4306b9f2d4b59396392c0a3f5622
hoadesign.09@gmail.com <hoadesign.09@gmail.com>
2023-03-07 05:32:38 +0700

    first update

---
+++ Screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, View, StyleSheet} from "react-native";
+
+function HomeScreen(): JSX.Element{
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+export default HomeScreen;
